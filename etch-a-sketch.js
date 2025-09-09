f8f23eca21d6b6dcd6e79adf6c86546a725d6032
joaquinauc <joaquins_urrea@hotmail.com>
2025-09-09 14:41:42 -0600

    Add size to the square and call function to create grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,14 +2,15 @@ const GRID_WIDTH = 960;
 
 function generateGrid(numberOfSquares)
 {   
-    squareSize = calculateSquareSize(numberOfSquares);
+    const squareSize = calculateSquareSize(numberOfSquares);
 
-    const gridContainer = document.querySelector(".conatiner")
-
+    const gridContainer = document.querySelector(".container")
+    
     for (let i = 0; i < (numberOfSquares**2); i++)
     {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.setAttribute("style", `height: ${squareSize}px; width: ${squareSize}px;`);
         gridContainer.appendChild(square);
     }
 }
@@ -18,3 +19,5 @@ function calculateSquareSize(numberOfSquares)
 {
     return Math.floor(GRID_WIDTH / numberOfSquares);
 }
+
+generateGrid(16);
